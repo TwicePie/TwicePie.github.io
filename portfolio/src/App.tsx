368ec493c65690d716af921a612a809236ee92a6
TwicePie <kknurarief@gmail.com>
2025-02-12 08:23:50 +0700

    Add Navbar component for improved site navigation

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import StarryBackground from './components/StarryBackground'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router basename="/">
       <div className="min-h-screen relative">
-        <div className="z-[20] relative">
+        <Navbar />
+        <div className="z-[20] relative pt-20">
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
---
+++ portfolio/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-4 left-0 right-0 z-50">
+      <div className="navbar bg-base-100 max-w-4xl mx-auto rounded-box shadow-lg bg-opacity-90 backdrop-blur-md">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </div>
+          <Link to="/" className="btn btn-ghost text-xl">Ocharu</Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-primary">Contact</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
